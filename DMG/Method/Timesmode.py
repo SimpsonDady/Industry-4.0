fbe2380bed51087b2a5106bfb3440d30eab739f6
Simpson <charlie40503@gmail.com>
2017-07-24 09:34:20 +0800

    新增存取絕對路徑

--- DMG/Method/Timesmode.py
+++ DMG/Method/Timesmode.py
@@ -9,65 +9,61 @@ class Timesmode:
         self.wb = self.ws.active
         self.open = load_workbook(plan_file)
         self.xlsx = self.open['Sheet1']
+        self.wb.append(['工具代碼', '件號', '工號', 'NC程式', '版別', '工作中心', '機台編號',
+                        '開始日期', '開始時間', '結束日期', '結束時間', '加工工時'])
         self.output()
 
     def output(self):
-        self.wb.append(['工具代碼', '件號', '工號', 'NC程式', '版別', '工作中心', '機台編號', '開始日期', '開始時間', '結束日期', '結束時間', '加工工時'])
-        code = self.inputdata[0].getProgramCode(0)
-        date = self.inputdata[0].getDate()
-        starttime = self.inputdata[0].getStartTime(0)
-        startday = self.inputdata[0].getDay()
-        startsecond = self.inputdata[0].getStartSecond(0)
-        endtime = self.inputdata[0].getEndTime(0)
-        endday = self.inputdata[0].getDay()
-        endsecond = self.inputdata[0].getEndSecond(0)
+        warm = 0
+        startcode = 0
+        startdate = 0
+        endcode = 0
+        enddate = 0
+        totaltime = 0
+        for i in range(len(self.inputdata)):
+            for j in range(self.inputdata[i].getProgramLength()):
+                if self.inputdata[i].getProgramCode(j) == 'TOOL-SL-D':
+                    continue
+                elif self.inputdata[i].getProgramCode(j) == 'WARM':
+                    warm = 2
+                    self.save(startdate, startcode, enddate, endcode)
+                elif self.inputdata[i].getProgramCode(j) == self.inputdata[enddate].getProgramCode(endcode) and not warm:
+                    startdate = i
+                    startcode = j
+                elif warm:
+                    startdate = i
+                    startcode = j
+                    enddate = i
+                    endcode = j
+                    warm -= 1
+                    if warm:
+                        self.save(startdate, startcode, enddate, endcode)
+                else:
+                    self.save(startdate, startcode, enddate, endcode)
+                    startdate = i
+                    startcode = j
+                    enddate = i
+                    endcode = j
+        self.save(startdate, startcode, enddate, endcode)
+        self.ws.save('C:\\Users\\User\\Desktop\\result\\time_model\\' + self.machine_name + '_TimeModel.xlsx')
+
+    def save(self, startdate, startcode, enddate, endcode):
         worknum = 'N/A'
         NC = 'N/A'
         workcenter = 'N/A'
         component = 'N/A'
         for i in range(2, self.xlsx.max_row):
-            if self.xlsx['G' + str(i)].value == self.inputdata[0].getComponent(0)\
-                    and self.xlsx['M' + str(i)].value == self.inputdata[0].getProgramCode(0):
+            if self.xlsx['G' + str(i)].value == self.inputdata[startdate].getComponent(startcode) \
+                    and self.xlsx['M' + str(i)].value == self.inputdata[startdate].getProgramCode(startcode):
                 worknum = self.xlsx['B' + str(i)].value
                 NC = self.xlsx['J' + str(i)].value
                 workcenter = self.xlsx['D' + str(i)].value
                 component = self.xlsx['G' + str(i)].value
-        temp = [self.inputdata[0].getProgramCode(0), component, worknum, NC,
-                self.inputdata[0].getVersion(0), workcenter, self.machine_name]
-        for i in self.inputdata:
-            for j in range(i.getProgramLength()):
-                if i.getProgramCode(j) == 'TOOL-SL-D.I' or i.getProgramCode(j) == 'WARM' or i.getProgramCode(j) == code:
-                    starttime = i.getStartTime(j)
-                    startday = i.getDay()
-                    startsecond = i.getStartSecond(j)
-                    if date != i.getDate():
-                        endsecond += 86400
-                    date = i.getDate()
-                else:
-                    worktime = (endsecond - startsecond) / 3600
-                    temp.extend([startday, starttime, endday, endtime, worktime])
-                    self.wb.append(temp)
-                    temp = []
-                    startday = i.getDay()
-                    starttime = i.getStartTime(j)
-                    startsecond = i.getStartSecond(j)
-                    code = i.getProgramCode(j)
-                    endtime = i.getEndTime(j)
-                    endday = i.getDay()
-                    endsecond = i.getEndSecond(j)
-                    worknum = 'N/A'
-                    NC = 'N/A'
-                    workcenter = 'N/A'
-                    component = 'N/A'
-                    for k in range(2, self.xlsx.max_row):
-                        if self.xlsx['G' + str(k)].value == i.getComponent(j) and self.xlsx[
-                                    'M' + str(k)].value == i.getProgramCode(j):
-                            worknum = self.xlsx['B' + str(k)].value
-                            NC = self.xlsx['J' + str(k)].value
-                            workcenter = self.xlsx['D' + str(k)].value
-                            component = self.xlsx['G' + str(k)].value
-                    temp = [i.getProgramCode(j), component, worknum, NC, i.getVersion(j), workcenter, self.machine_name]
-        worktime = (endsecond - startsecond) / 3600
-        temp.extend([startday, starttime, endday, endtime, worktime])
-        self.wb.append(temp)
-        self.ws.save(self.machine_name + '_TimeModel.xlsx')
+        time = (self.inputdata[enddate].getEndSecond(endcode)-self.inputdata[startdate].getStartSecond(startcode))/3600
+        if time < 0:
+            time += 86400
+        self.wb.append([self.inputdata[startdate].getProgramCode(startcode), component, worknum, NC,
+                        self.inputdata[startdate].getVersion(startcode), workcenter, self.machine_name,
+                        self.inputdata[startdate].getDay(), self.inputdata[startdate].getStartTime(startcode),
+                        self.inputdata[enddate].getDay(), self.inputdata[enddate].getEndTime(endcode),
+                        time])
--- Main.py
+++ Main.py
@@ -1,5 +1,9 @@
 from DMG.CreateDMG import CreateDMG
 from Graph.CreateGraph import CreateGraph
 
-DMG = [CreateDMG('DMG01_0703-0707.txt', 'DMG01_0703-0707.xlsx'), CreateDMG('DMG02_0703-0707.txt', 'DMG02_0703-0707.xlsx')]
+DMG = [
+    CreateDMG('C:\\Users\\User\\Google 雲端硬碟\\漢翔-Industry4.0\\2017-07\\DMG01_0708-0716.txt',
+              'C:\\Users\\User\\Google 雲端硬碟\\漢翔-Industry4.0\\2017-07\\DMG01_0703-0707.xlsx'),
+    CreateDMG('C:\\Users\\User\\Google 雲端硬碟\\漢翔-Industry4.0\\2017-07\\DMG02_0708-0716.txt',
+              'C:\\Users\\User\\Google 雲端硬碟\\漢翔-Industry4.0\\2017-07\\DMG02_0703-0707.xlsx')]
 graph = CreateGraph(DMG)
